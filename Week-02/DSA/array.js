9f70102ef08bb7602e220019b9d39218ef996c1f
Mazharul <mazharul.jatri@gmail.com>
2025-10-07 14:45:36 +0600

    Object and closer

---
+++ Week-02/DSA/array.js
@@ -0,0 +1,12 @@
+// Add elements to the end of the array
+let arr1 = [1, 2, 3]
+arr1.push(5)
+
+// Pop last element of the array
+arr1.pop()
+
+// Add element beginning of the array
+let arr2 = [1, 2, 3]
+arr2.unshift(1)
+
+console.log(arr2)
