cea37ed5911ac4724ed99dc32d33c393c9f7d3a8
Mazharul <mazharul.jatri@gmail.com>
2025-10-27 16:19:24 +0600

    auth and authorization

--- Week-05/node/authentication - authorization/index.js
+++ Week-05/node/authentication - authorization/index.js
@@ -1,7 +1,37 @@
 import express from "express"
+import mongoose from "mongoose";
 
+await mongoose.connect('mongodb://localhost/mongoplayground')
 const app = express()
+
+const userSchema = mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true,
+    },
+    password: {
+        required: true,
+        type: String,
+    }
+})
+const User = mongoose.model('User', userSchema)
+
 app.use(express.json())
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const user = await User.create(req.body)
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hello world')
@@ -10,6 +40,7 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3])
 })
+
 
 app.listen(4000, () => console.log('Server is running'))
 
